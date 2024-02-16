c5cc7c26ddfd905661d1471b09777f662f79ea8d
Gabriel  Juan Alejandro Juhasz <88912420+GabJaJ@users.noreply.github.com>
2024-02-16 01:57:25 -0300

    Actualizo CSS
    Actualizo:
    
    AnzenControlStock/AnzenControlStock/settings.py
    
    Responsabilidades:
    -Sincronizar los estilos CSS

--- AnzenControlStock/AnzenControlStock/settings.py
+++ AnzenControlStock/AnzenControlStock/settings.py
@@ -116,16 +116,20 @@ USE_TZ = True
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/5.0/howto/static-files/
 
-STATIC_URL = "/static/"
+CRISPY_TEMPLATE_PACK = 'bootstrap4'
+# Static files (CSS, JavaScript, Images)
+# https://docs.djangoproject.com/en/3.1/howto/static-files/
+
+STATIC_URL = '/static/'
 
 
 STATICFILES_DIRS = [
-    BASE_DIR / "static"
-    ]
+    BASE_DIR / "static",
+]
 
 STATIC_ROOT = (BASE_DIR/"asert/")
 
-# Default primary key field type
-# https://docs.djangoproject.com/en/5.0/ref/settings/#default-auto-field
+LOGIN_REDIRECT_URL = 'dashboard-index'
 
-DEFAULT_AUTO_FIELD = "django.db.models.BigAutoField"
+LOGIN_URL = 'user-login'
+
