5379713c9e188765a8d8b74906e5baecaf9cf728
Gabriel  Juan Alejandro Juhasz <88912420+GabJaJ@users.noreply.github.com>
2024-02-15 23:27:51 -0300

    Actualizo repositorio
    Actualizo:
    
    -AnzenControlStock/templates/partials
    -AnzenControlStock/templates/dashboard
    
    Responsabilidades:
    
    -Se crean las paginas de products.html y order.html
    -Se genera el navbar

--- AnzenControlStock/dashBoard/views.py
+++ AnzenControlStock/dashBoard/views.py
@@ -8,7 +8,11 @@ from django.http import HttpResponse
 def index(request):
     return render(request, 'dashboard/index.html')
 
-
-
 def staff(request):
     return render(request, 'dashboard/staff.html')
+
+def products(request):
+    return render(request, 'dashboard/products.html')
+
+def order(request):
+    return render(request, 'dashboard/order.html')
