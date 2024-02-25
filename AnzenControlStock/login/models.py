0be3cdd35b57d67e444b53d8ffcf1095f4ad9dfc
Jessica Armoa <85409054+jessica-armoa@users.noreply.github.com>
2024-02-25 15:10:02 -0300

    Creo y documento metodo para generar un nombre de archivo unico

--- AnzenControlStock/login/models.py
+++ AnzenControlStock/login/models.py
@@ -1,6 +1,10 @@
 from django.db import models
 from django.contrib.auth.models import AbstractUser
 
+import os
+import uuid
+from django.core.files.uploadedfile import SimpleUploadedFile
+
 # Create your models here.
 
 class CustomUser(AbstractUser):
@@ -32,6 +36,32 @@ class Empresa(models.Model):
         return any(
             self.foto_perfil.name.lower().endswith(ext) for ext in extensiones_validas
         )
+
+    # Genera un nombre único para el archivo utilizando UUID y conserva la extensión.
+    @classmethod
+    def generate_unique_filename(file):
+        """
+        Genera un nombre de archivo único para un archivo de imagen.
+
+        Parámetros:
+        - file (InMemoryUploadedFile): El archivo de imagen proporcionado desde el frontend,
+                                        típicamente obtenido con request.FILES.get("id").
+
+        Retorna:
+        - SimpleUploadedFile: Un nuevo archivo de imagen con un nombre único generado.
+
+        Uso en el Frontend:
+        -   foto_de_perfil = request.FILES.get("foto_de_perfil")    #Suponiendo que "foto_de_perfil"
+                                                                    sea el id del input tipo file
+            if foto_de_perfil:
+                foto_de_perfil = generate_unique_filename(foto_de_perfil) #Sobreescribir con el nombre generado
+
+            #Por ultimo debe pasarse todos los datos (incluido foto_de_perfil) al metodo del backend para
+            guardar todo en la base de datos
+        """
+        extension = os.path.splitext(file.name)[1]
+        unique_name = f"{uuid.uuid4()}{extension}"
+        return SimpleUploadedFile(unique_name, file.read())
 
 
 class Empleado(models.Model):
