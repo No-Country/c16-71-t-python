558607925018f1123479082e62602f2c803e0e6d
franc <franco@gmail.com>
2024-02-25 10:44:30 -0300

    actualizo frontEnd

--- AnzenControlStock/dashBoard/urls.py
+++ AnzenControlStock/dashBoard/urls.py
@@ -2,11 +2,8 @@ from django.urls import path
 from . import views
 
 urlpatterns = [
-    path('', views.index, name='dashboard-index'), 
-    path('inicio/', views.inicio, name='dashboard-inicio'), 
-    path('staff/', views.staff, name='dashboard-staff'),
-    path('products/', views.products, name='dashboard-products'),
-    path('order/', views.order, name='dashboard-order'),
+    path('', views.inicio, name='dashboard-inicio'), 
+    path('main/', views.main, name='dashboard-main'), 
     path('registro/', views.registro, name='dashboard-registro'), 
-    path('registro2/', views.registro2, name='dashboard-registro2'), 
+    path('registro2/', views.registro2, name='dashboard-registro2')
 ]
