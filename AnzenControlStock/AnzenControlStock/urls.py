4d40a313f84fbe98cb0c666960520072ccb5e6ba
franc <franco@gmail.com>
2024-02-26 20:59:20 -0300

    actualizo frontEnd

--- AnzenControlStock/AnzenControlStock/urls.py
+++ AnzenControlStock/AnzenControlStock/urls.py
@@ -19,9 +19,7 @@ from django.urls import path, include
 
 urlpatterns = [
     path("admin/", admin.site.urls),
-<<<<<<< HEAD
-     path("", include("login.urls")),
-=======
+
     path('', include('dashBoard.urls'))
->>>>>>> main
+
 ]
