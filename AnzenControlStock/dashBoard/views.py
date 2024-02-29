572a5c418a3eba87ea16bd353ee3dd554c250673
Jessica Armoa <85409054+jessica-armoa@users.noreply.github.com>
2024-02-29 18:24:10 -0300

    Ordeno los templates

--- AnzenControlStock/dashBoard/views.py
+++ AnzenControlStock/dashBoard/views.py
@@ -10,31 +10,6 @@ from django.contrib.auth import login, authenticate, get_user_model
 
 # Create your views here.
 
-def main(request):
-    if request.method == 'GET':
-        if request.session.get('id_user'):
-            return render(request, 'dashboard/dashboard.html', {'seccion_actual':'inicio'})
-        else:
-            return redirect('dashboard-inicio')
-
-def inventario(request):
-    if request.method == 'GET':
-        if request.session.get('id_user'):
-            messages.error(request, "Hola")
-            return render(request, 'dashboard/inventario.html')
-
-def staff(request):
-    if request.method == 'GET':
-        if request.session.get('id_user'):
-            return render(request, 'dashboard/staff.html')
-
-def products(request):
-    if request.method == 'GET':
-        if request.session.get('id_user'):
-            return render(request, 'dashboard/products.html')
-
-def order(request):
-    return render(request, 'dashboard/order.html')
 
 def inicio(request):
     if request.method == 'POST':
@@ -52,36 +27,38 @@ def inicio(request):
                 request.session["id_user"] = usuario.id
                 print ("Sesion iniciada "+ str(request.session.get("id_user")))
                 messages.success(request, "Sesión iniciada correctamente")
-                return redirect("dashboard-main")
+                return redirect("dashboard")
         else:
             messages.error(request, "Error, verifique sus datos")
-            return redirect("dashboard-main")
+            return redirect("inicio")
     else:
-        if not request.session.get('id_user'):
-            return render(request, 'dashboard/inicio.html')
+        return render(request, 'login/inicio.html')
 
 
 def registro(request):
     if request.method == 'GET':
-        return render(request, 'dashboard/registro.html')
+        return render(request, 'login/registro.html')
     if request.method == 'POST':
         # verificaciones
         if request.POST['password1'] == request.POST['password2'] and request.POST['nombre'] and request.POST['email']:
             new_user = CustomUser.register(request.POST['nombre'], request.POST['email'], request.POST['password1'])
             if new_user == -1:
                 print("El correo ya existe, mostrar un mensaje de error con toast")
+                messages.error(request, "Error, el email utilizado ya existe")
+                return redirect("registro")
             elif new_user == -2:
                 print("Ocurrio un error durante el registro")
+                messages.error(request, "Ocurrio un error durante el registro")
+                return redirect("registro")
             else:
                 print("El usuario ",new_user.nombre, "fue creado")
-                return redirect("dashboard-registro2", id_user=new_user.id) #Se le pasa el name del url
-            return HttpResponse("Hubo un problema durante el registro. Verifica tus datos.")
+                return redirect("registro2", id_user=new_user.id) #Se le pasa el name del url
 
 def registro2(request,id_user):
     if request.method == 'GET':
         return render(
             request,
-            "dashboard/registro2.html",
+            "login/registro2.html",
             {"id_user": id_user},
         )
     if request.method == 'POST':
@@ -105,16 +82,53 @@ def registro2(request,id_user):
 
         if new_empresa == -2:
             print("Ocurrio un error")
-            return redirect("dashboard-registro2", id_user=id_user)
+            messages.error(request, "Ocurrio un error")
+            return redirect("registro2", id_user=id_user)
         else:
             print("Se creo la empresa", new_empresa.nombre_de_la_empresa)
             # Iniciar sesion automaticamente
             request.session["id_user"] = id_user
             print ("Sesion iniciada "+ str(request.session.get("id_user")))
             messages.success(request, "Sesión iniciada correctamente")
-            return redirect("dashboard-main")
+            return redirect("dashboard")
 
 
 def cerrar_sesion(request):
     request.session["id_user"] = None
-    return redirect("dashboard-inicio")
+    return redirect("inicio")
+
+
+def main(request):
+    if request.method == "GET":
+        if request.session.get("id_user"):
+            return render(
+                request, "dashboard/dashboard.html", {"seccion_actual": "dashboard"}
+            )
+        else:
+            return redirect("inicio")
+
+
+def inventario(request):
+    if request.method == "GET":
+        if request.session.get("id_user"):
+            return render(
+                request, "inventario/inventario.html", {"seccion_actual": "inventario"}
+            )
+
+
+def inventario(request):
+    if request.method == "POST":
+        return HttpResponse("Hola")
+    if request.method == "GET":
+        id_user = request.session.get("id_user")
+        if id_user:
+            return render(
+                request, "inventario/inventario.html", {"seccion_actual": "inventario", "id_user": id_user}
+            )
+        else:
+            return redirect("dashboard-inicio")
+
+
+def crear_producto(request):
+    if request.method == "GET":
+        return render(request, "inventario/crear_producto.html")
