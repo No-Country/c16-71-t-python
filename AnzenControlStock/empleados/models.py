02621452ba863056d4f7dd43b6a6a85946384f9d
Gabriel  Juan Alejandro Juhasz <88912420+GabJaJ@users.noreply.github.com>
2024-03-05 12:52:06 -0300

    Actualizo vista_empleado: eliminar_empleado
    Actualizo:
    
    -AnzenControlStock/templates/dashboard/vista_empleados.html
    -AnzenControlStock/empleados/models.py

--- AnzenControlStock/empleados/models.py
+++ AnzenControlStock/empleados/models.py
@@ -6,5 +6,22 @@ class Empleado(models.Model):
     mailEmpleado=models.CharField(max_length=32)
     telefonoEmpleado=models.IntegerField()
     password=models.CharField(max_length=32
+                        
+    @classmethod
+    def actualizar_empleado(
+        cls,
+        nombreEmpleado,
+        mailEmpleado,
+        telefonoEmpleado,
+        password,
+        
+    ):
+    
+        except cls.DoesNotExist:
+            raise Exception("El Empleado no esta registrado.")
+                        
+
+    def eliminar_producto(self):
+            self.delete()
     
 
