1c53d4f9d96086fa4b48a24c20cbc17e29a9a84e
Gabriel  Juan Alejandro Juhasz <88912420+GabJaJ@users.noreply.github.com>
2024-03-05 11:59:02 -0300

    Actiualizo empleado models
    Actualizo:
    
    -AnzenControlStock/empleados/models.py

---
+++ AnzenControlStock/empleados/models.py
@@ -0,0 +1,10 @@
+from django.db import models
+
+# Create your models here.
+class Empleado(models.Model):
+    nombreEmpleado=models.CharField(max_length=32)
+    mailEmpleado=models.CharField(max_length=32)
+    telefonoEmpleado=models.IntegerField()
+    password=models.CharField(max_length=32
+    
+
